36a99231935f38b901cdff69069e71335a7c37b7
Yeonho Jang <siabard@gmail.com>
2019-12-26 21:43:42 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+const App = () => {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess Game" />
+      <StartGameScreen />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export default App;
